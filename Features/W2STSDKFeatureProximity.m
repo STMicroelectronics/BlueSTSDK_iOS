7716240c274860a9bd7b07cd262933b8ccbb2dfd
Giovanni Visentini <giovannivisentini@ASTs-MacBook-Pro.local>
2015-05-12 11:18:08 +0200

    add proximity to the cube

--- Features/W2STSDKFeatureProximity.m
+++ Features/W2STSDKFeatureProximity.m
@@ -15,7 +15,7 @@
 #define FEATURE_NAME @"Proximity"
 #define FEATURE_UNIT @"mm"
 #define FEATURE_MIN @0
-#define FEATURE_MAX @1000
+#define FEATURE_MAX @350
 #define FEATURE_TYPE W2STSDKFeatureFieldTypeUInt16
 #define FEATURE_OUT_OF_RANGE_VALUE 510
 static NSArray *sFieldDesc;
