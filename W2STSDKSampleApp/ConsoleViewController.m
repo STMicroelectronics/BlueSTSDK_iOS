e030040b219498d60f06a11d430a733df65d0b78
Giovanni Visentini <giovannivisentini@asts-mbp.ast.agr.st.com>
2015-08-04 15:30:24 +0200

    add comments

--- W2STSDKSampleApp/ConsoleViewController.m
+++ W2STSDKSampleApp/ConsoleViewController.m
@@ -8,105 +8,160 @@
 
 #import "ConsoleViewController.h"
 
+/**
+ *  style used for the error message
+ */
+static NSDictionary *sErrorAttribute;
 
-static NSDictionary *errorAttribute;
-static NSDictionary *outAttribute;
-static NSDictionary *inAttribute;
+/**
+ *  style used for the output message
+ */
+static NSDictionary *sOutAttribute;
+
+/**
+ *  style used for the input message
+ */
+static NSDictionary *sInAttribute;
+
+/**
+ *  object used for print the message date
+ */
+static NSDateFormatter *sDateFormatter;
+
 
 @interface ConsoleViewController () <W2STSDKDebugOutputDelegate,
     UITextFieldDelegate>
 @end
 
 @implementation ConsoleViewController{
+    
+    /**
+     *  displayed string with all the message send/received to/from the board
+     */
     NSMutableAttributedString *mDisplayString;
-    NSDateFormatter *mDateFormatter;
 }
 
+/**
+ *  initialize the private static variable
+ */
 +(void)initialize{
     if(self == [ConsoleViewController class]){
-        errorAttribute = @{
-                           NSForegroundColorAttributeName: [UIColor redColor]
-                           };
-        outAttribute = @{
-                         NSForegroundColorAttributeName: [UIColor blueColor]
+        // error message display with read color
+        sErrorAttribute = @{
+                            NSForegroundColorAttributeName: [UIColor redColor]
+                            };
+        // send message display with blue color
+        sOutAttribute = @{
+                          NSForegroundColorAttributeName: [UIColor blueColor]
+                          };
+        // received message display with green color
+        sInAttribute = @{
+                         NSForegroundColorAttributeName: [UIColor greenColor]
                          };
-        inAttribute = @{
-                        NSForegroundColorAttributeName: [UIColor greenColor]
-                        };
+        
+        // object that will format the message received time
+        sDateFormatter = [[NSDateFormatter alloc] init];
+        sDateFormatter.timeStyle = NSDateFormatterMediumStyle;
+        sDateFormatter.dateStyle = NSDateFormatterMediumStyle;
     }//if
-}
+}//initialize
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect {
- // Drawing code
- }
- */
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     mDisplayString = [[NSMutableAttributedString alloc] init];
-    
-    _userText.delegate=self;
-    
-    
-    mDateFormatter = [[NSDateFormatter alloc] init];
-    mDateFormatter.timeStyle = NSDateFormatterMediumStyle;
-    mDateFormatter.dateStyle = NSDateFormatterMediumStyle;
-}
+    self.userText.delegate=self;
+}//viewDidLoad
 
+/**
+ *  when the view appear, register this class as debug delegate + display the
+ *  text input
+ *
+ *  @param animated <#animated description#>
+ */
 -(void)viewDidAppear:(BOOL)animated{
     [super viewDidAppear:animated];
-    _debugInterface.delegate=self;
-    [_userText becomeFirstResponder];
-}
+    self.debugInterface.delegate=self;
+    [self.userText becomeFirstResponder];
+}//viewDidAppear
 
+/**
+ * when the view disappear remove the debug listener
+ *
+ *  @param animated <#animated description#>
+ */
 -(void)viewDidDisappear:(BOOL)animated{
     [super viewDidDisappear:animated];
-    _debugInterface.delegate=nil;
-}
+    self.debugInterface.delegate=nil;
+}//viewDidDisappear
 
+/**
+ *  when the user press the return button, send the write message and reset the
+ *  text field and hide the keyboard
+ *
+ *  @param textField text field that generate the callback
+ *
+ *  @return true
+ */
 - (BOOL)textFieldShouldReturn:(UITextField *)textField{
     [_debugInterface writeMessage: textField.text];
     textField.text=@"";
+    [self.view endEditing:YES];
     return true;
-}
+}//textFieldShouldReturn
 
-
+/**
+ *  display a message received from the device
+ *
+ *  @param debug object that send the message
+ *  @param msg   message received from the board
+ */
 -(void) debug:(W2STSDKDebug*)debug didStdOutReceived:(NSString*) msg{
     
     NSString *raw = [NSString stringWithFormat:@"%@ <- %@\n",
-                     [mDateFormatter stringFromDate:[NSDate date]],  msg];
+                     [sDateFormatter stringFromDate:[NSDate date]],  msg];
     NSAttributedString *temp = [[NSAttributedString alloc] initWithString:raw
-                                                               attributes:outAttribute];
+                                                               attributes:sOutAttribute];
     [mDisplayString appendAttributedString:temp];
     dispatch_async(dispatch_get_main_queue(),^{
         _console.attributedText = mDisplayString;
     });
-}
+}//didStdOutReceived
 
+/**
+ *  display an error message received from the device
+ *
+ *  @param debug object that send the message
+ *  @param msg   error message received from the board
+ */
 -(void) debug:(W2STSDKDebug*)debug didStdErrReceived:(NSString*) msg{
     NSString *raw = [NSString stringWithFormat:@"%@ <- %@\n",
-                     [mDateFormatter stringFromDate:[NSDate date]],  msg];
+                     [sDateFormatter stringFromDate:[NSDate date]],  msg];
     NSAttributedString *temp = [[NSAttributedString alloc] initWithString:raw
-                                                               attributes:errorAttribute];
+                                                               attributes:sErrorAttribute];
     [mDisplayString appendAttributedString:temp];
     dispatch_async(dispatch_get_main_queue(),^{
         _console.attributedText = mDisplayString;
     });
-}
+}//didStdErrReceived
 
+/**
+ *  display a message that is send to the device
+ *
+ *  @param debug object where we send the message
+ *  @param msg   message send
+ *  @param error error happen during the sending or nil is the message was correctly send
+ */
 -(void) debug:(W2STSDKDebug*)debug didStdInSend:(NSString*) msg error:(NSError*)error{
     NSString *raw = [NSString stringWithFormat:@"%@ -> %@\n",
-                     [mDateFormatter stringFromDate:[NSDate date]],  msg];
+                     [sDateFormatter stringFromDate:[NSDate date]],  msg];
     NSAttributedString *temp = [[NSAttributedString alloc] initWithString:raw
-                                                               attributes:inAttribute];
+                                                               attributes:sInAttribute];
     [mDisplayString appendAttributedString:temp];
     dispatch_async(dispatch_get_main_queue(),^{
         _console.attributedText = mDisplayString;
     });
-}
+}//didStdInSend
 
 @end
 
